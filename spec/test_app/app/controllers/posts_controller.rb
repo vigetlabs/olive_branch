0bbaa78a54090e9dcef6e7c4371ef48bd9231215
Kyle Welsby <kyle@mekyle.com>
2019-02-21 11:41:03 +0000

    Cleanup Rails TestApp and remove sqlite3 dev dep
    This gem can work with the bare bones of Ruby on Rails and thus stripping back the railties makes the repository lighter and potentially quicker in testing.
    
    Useful test commands:
    
        bundle exec appraisal rspec
        bundle exec rspec

--- spec/test_app/app/controllers/posts_controller.rb
+++ spec/test_app/app/controllers/posts_controller.rb
@@ -1,12 +1,15 @@
+# PostsController
 class PostsController < ApplicationController
-  COMPLEX_RESPONSE ||= JSON.parse(File.read(Rails.root.join("example_responses/complex.json")))
+  COMPLEX_RESPONSE ||= JSON.parse(
+    File.read(Rails.root.join('example_responses/complex.json'))
+  )
 
   def update
-    request.env["params_spy"] = params
+    request.env['params_spy'] = params
 
     payload = {
       post_author_name: params[:post][:author_name],
-      category_filter_name: (params[:category_filter] || {})[:category_name],
+      category_filter_name: (params[:category_filter] || {})[:category_name]
     }
 
     render json: payload
--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -1,30 +1,16 @@
 require_relative 'boot'
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
-begin
-  require "active_job/railtie"
-rescue LoadError
-end
-require "sprockets/railtie"
-require "olive_branch"
+require 'action_controller/railtie'
+require 'olive_branch'
 
 Bundler.require(*Rails.groups)
 
 module TestApp
+  # Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    if config.respond_to?(:load_defaults)
-      config.load_defaults 5.1
-    end
+    config.load_defaults 5.1 if config.respond_to?(:load_defaults)
 
     config.middleware.use OliveBranch::Middleware
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
